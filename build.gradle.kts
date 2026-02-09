1004fb5f6a818ceb9a20be99656d1a1567806c07
Alejandro C <211375403+wikimicroondas@users.noreply.github.com>
2026-02-09 16:54:35 +0100

    estoy en el paso 7 me acabo de acordar de la repo

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "com.alejandrocamino.tema4gradle"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(platform("dev.langchain4j:langchain4j-bom:1.10.0"))
+    implementation("dev.langchain4j:langchain4j-open-ai")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/alejandrocamino/tema4gradle/Main.java
@@ -0,0 +1,34 @@
+package com.alejandrocamino.tema4gradle;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // El TOKEN no es necesario para interactuar con modelos locales
+        final String TOKEN = "PEGA_AQUI_TU_TOKEN";
+        var model = OpenAiChatModel.builder()
+                .baseUrl("http://localhost:11434/v1")
+                .apiKey(TOKEN)
+                .modelName("gemma:2b")
+                .build();
+        List<ChatMessage> history = new ArrayList<>();
+        // Interacción 1
+        history.add(new UserMessage("Hola, soy Germán"));
+        AiMessage respuesta = model.chat(history).aiMessage();
+        history.add(respuesta);
+        // Interacción 2
+        history.add(new UserMessage("¿Recuerdas cómo me llamo?"));
+
+
+
+        respuesta = model.chat(history).aiMessage();
+        history.add(respuesta);
+        System.out.println(respuesta.text());
+    }
+}
